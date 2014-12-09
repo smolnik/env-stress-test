ce9cb2f9b0dc4ba6f7a67f74e50559b769aab1e9
smolnik <smolnik@wp.pl>
2014-12-09 01:11:19 +0100

    next portion

--- src/main/java/net/adamsmolnik/ws/WsClient.java
+++ src/main/java/net/adamsmolnik/ws/WsClient.java
@@ -1,41 +1,68 @@
 package net.adamsmolnik.ws;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
 import javax.websocket.ClientEndpoint;
+import javax.websocket.ClientEndpointConfig;
 import javax.websocket.CloseReason;
-import javax.websocket.OnClose;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
+import javax.websocket.ContainerProvider;
+import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
 import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
 
 /**
  * @author ASmolnik
  *
  */
-@ClientEndpoint
 public class WsClient {
-    
-    private final String url;
-    
-    public WsClient(String url){
-        this.url = url;
-    }
 
-    @OnOpen
-    public void onOpen(Session session) {
-        try {
-            session.getBasicRemote().sendText("start");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    class WsEndpoint extends javax.websocket.Endpoint {
+
+        public void onOpen(Session session, EndpointConfig config) {
+        }
+
+        public void onClose(Session session, CloseReason closeReason) {
+        }
+
+        public void onError(Session session, Throwable throwable) {
         }
     }
 
-    @OnMessage
-    public void onMessage(String message, Session session) {
+    class WsMessageHandler implements MessageHandler.Whole<String> {
+
+        @Override
+        public void onMessage(String message) {
+            System.out.println("Received: " + message);
+        }
+
     }
 
-    @OnClose
-    public void onClose(Session session, CloseReason closeReason) {
+    private final Session session;
+
+    public WsClient(String url) {
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        try {
+            session = container.connectToServer(new WsEndpoint(), ClientEndpointConfig.Builder.create().build(), new URI(url));
+            session.addMessageHandler(new WsMessageHandler());
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
     }
 
+    public void send(String message) {
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        WsClient ws = new WsClient("ws://wdsc.adamsmolnik.com/webclient/ws");
+        String dsElb = "elb-student001-353948561.us-east-1.elb.amazonaws.com";
+        ws.send("launch;" + dsElb + ";largefiles/file_sizedOf10000000;100;0;10");
+        TimeUnit.SECONDS.sleep(30);
+    }
 }
